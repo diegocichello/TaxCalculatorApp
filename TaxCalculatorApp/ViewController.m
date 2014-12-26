5029c16f93a92b2f70ee61e61f361b09790ad542
Diego Cichello <diegocichello@Diegos-MacBook-Pro.local>
2014-12-26 16:53:51 -0200

    First commit

--- TaxCalculatorApp/ViewController.m
+++ TaxCalculatorApp/ViewController.m
@@ -10,14 +10,64 @@
 
 @interface ViewController ()
 
+@property double ca_tax;
+@property double chi_tax;
+@property double ny_tax;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.ca_tax = 7.5/100;
+    self.chi_tax = 9.25/100;
+    self.ny_tax = 4.5/100;
+    self.resultLabel.text =@"";
+
 }
+- (IBAction)onButtonTapped:(id)sender {
+
+    NSLog(@"Ok1");
+
+
+    NSString *tfContent = self.priceTextField.text;
+
+
+     double value = tfContent.doubleValue;
+     double resultValue = 0;
+
+    NSLog(@"%ld",(long)self.segmentedControl.selectedSegmentIndex);
+
+
+
+     switch (self.segmentedControl.selectedSegmentIndex)
+     {
+         case 0:
+             NSLog(@"Zero");
+             resultValue = value * self.ca_tax; break;
+         case 1:
+             NSLog(@"One");
+             resultValue = value * self.chi_tax; break;
+         case 2:
+             NSLog(@"Two");
+             resultValue = value * self.ny_tax; break;
+         default:
+             NSLog(@"Bug");
+
+     }
+
+     NSLog(@"%f",resultValue);
+
+     self.resultLabel.text =  [NSString stringWithFormat:@"%.2f",resultValue];
+
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
